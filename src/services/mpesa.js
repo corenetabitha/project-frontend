01915043d3d695de0cdfe45e43475b35903381c3
christine-muigai <git@github.com>
2025-07-17 17:10:23 +0300

    Auto commit

--- src/services/mpesa.js
+++ src/services/mpesa.js
@@ -1,5 +1,22 @@
+// src/utils/mpesa.js
 import axios from 'axios';
 
-export const payWithMpesa = (phone, amount) => {
-  return axios.post('/api/cart/mpesa/checkout/', { phone, amount });
+export const payWithMpesa = async (phone, amount, cartId) => {
+  const token = localStorage.getItem("token"); // JWT for auth
+  try {
+    const response = await axios.post(
+      '/api/cart/mpesa/checkout/',
+      { phone, amount, cart_id: cartId },
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          'Content-Type': 'application/json'
+        }
+      }
+    );
+    return response.data; // successful payment message
+  } catch (error) {
+    console.error("M-Pesa payment failed", error);
+    throw error;
+  }
 };
