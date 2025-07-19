205f6feecf176eefbde2df435c3adfa4474dbd9b
Vickitah <victor.mutuma@student.moringaschool.com>
2025-07-19 03:28:18 +0300

    Add ProtectedRoute component for protection

---
+++ src/components/ProtectedRoute.jsx
@@ -0,0 +1,18 @@
+import { useSelector } from "react-redux";
+import { Navigate } from "react-router-dom";
+
+const ProtectedRoute = ({ children, requiredRole }) => {
+  const { user, isAuthenticated } = useSelector((state) => state.auth);
+
+  if (!isAuthenticated || !user) {
+    return <Navigate to="/login" replace />;
+  }
+
+  if (requiredRole && user.role !== requiredRole) {
+    return <Navigate to="/unauthorized" replace />;
+  }
+
+  return children;
+};
+
+export default ProtectedRoute;
