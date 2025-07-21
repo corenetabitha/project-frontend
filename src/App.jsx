3f5a25fec06d91c128c11b12f303b14a858db7de
christine-muigai <git@github.com>
2025-07-21 15:38:14 +0300

    Auto commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,18 @@
-import React, { useEffect, useState } from 'react';
+// src/App.jsx
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Cart from './pages/Cart';
 import CartProvider from './context/CartProvider';
-import CartSection from './components/CartSection';
-import { fetchBooks } from './services/api';
 
 function App() {
-  const [books, setBooks] = useState([]);
-
-  useEffect(() => {
-    fetchBooks().then(setBooks);
-  }, []);
-
   return (
     <CartProvider>
-      <div className="p-8">
-        <h1 className="text-3xl font-bold">Booked Bookstore</h1>
-        <div className="mt-4 grid grid-cols-2 gap-4">
-          {books.map(book => (
-            <div key={book.id} className="border p-4">
-              <h3>{book.title}</h3>
-              <p>${book.price}</p>
-              <p>Available for: {book.available_for}</p>
-            </div>
-          ))}
-        </div>
-
-        <div className="grid grid-cols-2 gap-8 mt-8">
-          <CartSection type="purchase" />
-          <CartSection type="borrow" />
-        </div>
-      </div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Cart />} />
+          
+        </Routes>
+      </Router>
     </CartProvider>
   );
 }
