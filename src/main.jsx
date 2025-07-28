f32b6cb3a36675eb407f7c6422720a22e30161b1
Corene <corenealways54@gmail.com>
2025-07-28 12:13:13 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store";
 import "./index.css";
@@ -12,9 +11,7 @@ if (root) {
   ReactDOM.createRoot(root).render(
     <React.StrictMode>
       <Provider store={store}>
-        <BrowserRouter>
           <App />
-        </BrowserRouter>
       </Provider>
     </React.StrictMode>
   );
